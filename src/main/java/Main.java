8fb9381281e2903d4bddfef61b897c00eefc4261
krugli <kgantsev@gmail.com>
2022-04-21 22:46:21 +0500

    Добавили окно для интерфейса.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
-
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-
+        JFrame main_GUI = new JFrame("Victory");
+        main_GUI.setTitle ("Calc_GI");
+        main_GUI.setBounds(500,400,400,500);
+        main_GUI.setResizable(false);
+        main_GUI.setVisible(true);
     }
 }
