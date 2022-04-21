768231ca2313e07233706193fc80b48e40f078c6
krugli <kgantsev@gmail.com>
2022-04-22 01:35:55 +0500

    Отредактировал координаты расположения интерфейса

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args){
         JFrame main_GUI = new JFrame("Victory");
         main_GUI.setTitle ("Calc_GI");
-        main_GUI.setBounds(600,400,900,600);
+        main_GUI.setBounds(100,100,1150,600);
         main_GUI.setResizable(false);
 
         JPanel main_panel = new JPanel();
@@ -16,24 +16,29 @@ public class Main {
         main_GUI.add(main_panel);
 
         JLabel label1 = new JLabel("Откуда");
-        label1.setBounds(30,50,300,30);
+        label1.setBounds(150,210,100,30);
+        label1.setFont(new Font("Times New Roman", Font.PLAIN, 18));
         main_panel.add(label1);
 
         JLabel label2 = new JLabel("Куда");
-        label2.setBounds(200,50,300,30);
+        label2.setBounds(350,210,100,30);
+        label2.setFont(new Font("Times New Roman", Font.PLAIN, 18));
         main_panel.add(label2);
 
         JLabel label3 = new JLabel("Вес, кг");
-        label3.setBounds(370,50,300,30);
+        label3.setBounds(550,210,100,30);
+        label3.setFont(new Font("Times New Roman", Font.PLAIN, 18));
         main_panel.add(label3);
 
         JLabel label4 = new JLabel("Объём, м3");
-        label4.setBounds(540,50,300,30);
+        label4.setBounds(750,210,100,30);
+        label4.setFont(new Font("Times New Roman", Font.PLAIN, 18));
         main_panel.add(label4);
 
         JLabel label5 = new JLabel("ПРОСТОЙ КАЛЬКУЛЯТОР ДЛЯ РАСЧЕТА СТОИМОСТИ ПЕРЕВОЗКИ ГРУЗА");
-        label5.setBounds(200,0,600,30);
+        label5.setBounds(150,170,800,30);
         label5.setAlignmentX(Component.CENTER_ALIGNMENT);
+        label5.setFont(new Font("Times New Roman", Font.PLAIN, 22));
         main_panel.add(label5);
 
         String[] items = {
@@ -43,7 +48,8 @@ public class Main {
         };
         JComboBox<String> combo_tarif = new JComboBox<>(items);
         combo_tarif.setSelectedItem(0);
-        combo_tarif.setBounds(30,90,150,30);
+        combo_tarif.setBounds(150,240,150,30);
+        combo_tarif.setFont(new Font("Times New Roman", Font.PLAIN, 18));
         ActionListener actionListener = e -> {
 
         };
@@ -61,7 +67,8 @@ public class Main {
             };
             JComboBox<String> combo_tarif2 = new JComboBox<>(items2);
             combo_tarif2.setSelectedItem(0);
-            combo_tarif2.setBounds(200, 90, 150, 30);
+            combo_tarif2.setBounds(350, 240, 150, 30);
+            combo_tarif2.setFont(new Font("Times New Roman", Font.PLAIN, 18));
             ActionListener actionListener2 = e -> {
 
             };
@@ -75,30 +82,33 @@ public class Main {
             JLabel labelOutput = new JLabel("Результат:");
             labelOutput.setBounds(30, 300, 250, 30);
             labelOutput.setVisible(false);
+            labelOutput.setFont(new Font("Times New Roman", Font.PLAIN, 18));
             main_panel.add(labelOutput);
         }
 
         //Текстовое поле "Вес"
         {
             TextField weight = new TextField("Введите вес (КГ)");
-            weight.setBounds(370, 90, 150, 30);
+            weight.setBounds(550, 240, 150, 30);
             weight.setVisible(true);
+            weight.setFont(new Font("Times New Roman", Font.PLAIN, 16));
             main_panel.add(weight);
         }
 
         //Текстовое поле "Объём"
         {
-            TextField volume = new TextField("Длина, Ширина, Высота (СМ)");
-            volume.setBounds(540, 90, 200, 30);
+            TextField volume = new TextField("Длина, Ширина, Высота (см)");
+            volume.setBounds(750, 240, 240, 30);
             volume.setVisible(true);
+            volume.setFont(new Font("Times New Roman", Font.PLAIN, 16));
             main_panel.add(volume);
         }
 
         JButton button_create = new JButton("РАССЧИТАТЬ СТОИМОСТЬ");
-        button_create.setBounds(280,140,300,40);
+        button_create.setBounds(350,290,350,50);
         button_create.setBackground(new Color(255, 147, 3));
         button_create.setForeground(Color.white);
-        button_create.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        button_create.setFont(new Font("Times New Roman", Font.PLAIN, 24));
         ActionListener actionListener3 = e -> {
 
         };
