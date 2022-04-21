9e2f87c5ba06605a7a81996ac3642e30c95c1231
ren-ilyasov <ren.ilyasov@yandex.ru>
2022-04-22 00:00:44 +0500

    Добавлена надпись и выпадающий список

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args){
@@ -11,5 +14,24 @@ public class Main {
         JPanel main_panel = new JPanel();
         main_panel.setLayout(null);
         main_GUI.add(main_panel);
+
+        JLabel laba_info = new JLabel("Надпись");
+        laba_info.setBounds(60,0,300,30);
+        main_panel.add(laba_info);
+
+        String[] items = {
+                "Уфа",
+                "Абакан",
+                "Урюпинск"
+        };
+        JComboBox<String> combo_tarif = new JComboBox<>(items);
+        combo_tarif.setSelectedItem(0);
+        combo_tarif.setBounds(30,40,150,30);
+        ActionListener actionListener = e -> {
+
+        };
+        combo_tarif.setBackground(new Color(154,132,223));
+        combo_tarif.addActionListener(actionListener);
+        main_panel.add(combo_tarif);
     }
 }
